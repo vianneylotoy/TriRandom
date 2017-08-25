2c6d7458b6bb8b3b94b9bea727d7c94b2cd502a0
Vianney Lotoy <vianneylotoy@users.noreply.github.com>
2017-08-25 23:20:46 +0200

    Computer python class

---
+++ computer.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+class Computer:
+	#Attribute of instances of the class
+	def __init__(self):
+		self.PowerOn = 0.3
+		self.PowerOff = 0.5
+		self.DelayOn = 0.5
+		self.DelayOff = 0.0
+		self.Power = 1
+		self.Mbusy = []
+		
+		
+	
+			
+			
+	
+		
