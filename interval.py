c36a8c7b34a212cf9076a67683f271f9373679ee
Vianney Lotoy <vianneylotoy@users.noreply.github.com>
2017-08-25 23:20:01 +0200

    Intervals python class

---
+++ interval.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from random import randint,randrange
+
+class Intervalle:
+	
+	#Attribute of instances of the class
+	def __init__(self):
+		self.st = 0      # st: start time of the interval,
+		self.et = 0      # et: end time of the interval
+		self.Φ = 0       # Φ(t): Amount of energy in interval at t time
+		
+			
+
+			
+				
+			
+	
+	
+	
+			
+		
+	
